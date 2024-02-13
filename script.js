8959dca441c44f75686650a3e8a6e11a7e68a018
suki3174 <153454347+suki3174@users.noreply.github.com>
2024-02-13 23:31:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  // Set the URL of the page you want to navigate to
+  let nextPageUrl = "album.html";
+
+  // Redirect to the next page
+  window.location.href = nextPageUrl;
+});
+
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
